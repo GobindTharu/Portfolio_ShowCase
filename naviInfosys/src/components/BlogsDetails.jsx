fe93dac4469afec1b9df3c8cbb342a1b82b4cb5a
BalGobind Chaudhary <gobind9800@gmail.com>
2025-08-17 10:06:53 +0545

    changed Folder name

--- naviInfosys/src/components/BlogsDetails.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BlogsDetails = () => {
-  return <div className="flex justify-center items-center text-white">Blogs Detail Section</div>;
-};
-
-export default BlogsDetails;
